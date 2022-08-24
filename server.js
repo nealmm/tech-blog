fe926404eff0353bdca4ed512caa7b3c0637e9d3
Matt Neal <neal.matt.m@gmail.com>
2022-08-23 22:28:46 -0400

    setup handlebars

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express   = require('express')
-const sequelize = require('./config/connection')
+const exphbs    = require('express-handlebars')
 const path      = require('path')
+const routes    = require('./controllers')
+const sequelize = require('./config/connection')
 
 const app = express()
+const hbs = exphbs.create({})
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(process.env.PORT || 3001, () => console.log('Now listening'))
